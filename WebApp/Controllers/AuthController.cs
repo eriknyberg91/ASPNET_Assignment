e590b0b644f4b4d687fb4b034b3e5da9140052d5
Erik <erikmikaelnyberg@gmail.com>
2025-03-24 11:56:44 +0100

    Add project files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [Route("projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
