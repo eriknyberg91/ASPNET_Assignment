2579dbed44df6ad3caa21d11afaf1d352c85208b
Erik <erikmikaelnyberg@gmail.com>
2025-04-14 12:44:48 +0200

    Add modal for signing out user, small fixes for lists / showing user info

---
+++ Business/Models/EditProjectForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models;
+
+public class EditProjectForm
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Project Name", Prompt = "Enter project name")]
+    [DataType(DataType.Text)]
+    [Required(ErrorMessage = "Project name is required")]
+    public string ProjectName { get; set; } = null!;
+}
--- WebApp/ViewModels/ProjectsViewModel.cs
+++ WebApp/ViewModels/ProjectsViewModel.cs
@@ -6,5 +6,6 @@ namespace WebApp.ViewModels;
 public class ProjectsViewModel
 {
     public AddProjectForm AddProjectForm { get; set; } = new AddProjectForm();
+    public EditProjectForm EditProjectForm { get; set; } = new EditProjectForm();
     public IEnumerable<ProjectEntity> Projects { get; set; } = new List<ProjectEntity>();
 }
